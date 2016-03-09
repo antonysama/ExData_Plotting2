4ae4eed1bd8f670f1cb4c578911c18e442822476
Antony Samarawickrema <antonysama@gmail.com>
2016-03-08 23:54:57 -0700

    Update ExploratoryAssig2.R

--- ExploratoryAssig2.R
+++ ExploratoryAssig2.R
@@ -10,16 +10,16 @@ e<-pm25%>%
   group_by(year)%>%
   summarise_each(funs(sum, mean))
 with(e, plot(year, sum)
-#Q2Baltimore
-q2<-pm25[pm25$fips=="24510",]
-q2<-q2[,c(4,6)]
-library("dplyr")
-q2<-q2%>%
-  group_by(year)%>%
-  summarise_each(funs(sum, mean))
-with(q2, plot(year, sum))
-#Q3
-
-library("ggplot2")
-qplot(Emissions, year, data=pm25, facets=.~type)
-     
+     #Q2Baltimore
+     q2<-pm25[pm25$fips=="24510",c(4:6)]
+     q2<-q2[,c(4,6)]
+     library("dplyr")
+     q2<-q2%>%
+       group_by(year,type)%>%
+       summarise_each(funs(sum))
+     with(q2, plot(year, Emissions))
+     #Q3
+     install.packages("ggplot2")
+     library("ggplot2")
+     qplot(year,Emissions, data=q2, facets=.~type, geom = c("point","smooth"))
+     qplot(year, data=q2, facets=type~., binwidth=2)
